a4084d37d0189c5fcc3f3d5f89004dcd9f3bcf35
Jasmin Begic <jasmin.begic@atlantbh.com>
2016-09-27 16:37:16 +0200

    Added player create API, added heroku config

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,9 +1,5 @@
 package controllers;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-
 import models.MatchScore;
 import models.Player;
 import play.data.DynamicForm;
@@ -13,60 +9,34 @@ import play.mvc.Result;
 import services.TennisGameService;
 import views.html.index;
 
+import java.util.List;
+
 
 public class HomeController extends Controller {
 
 	public Result index() {
-		
-		List<MatchScore> latestScores = MatchScore.find.orderBy("creationTs desc").setMaxRows(5).fetch("playerOne").fetch("playerTwo").findList();
+
+		List<MatchScore> latestScores = MatchScore.find.orderBy("creationTs desc")
+				.setMaxRows(5).fetch("playerOne").fetch("playerTwo").findList();
 
 		latestScores = MatchScore.populateTimeSincePP(latestScores);
-		
+
 		List<Player> allPlayers = Player.find.order("ranking desc").order("position").findList();
         return ok(index.render(allPlayers, latestScores));
     }
-    
+
     public Result addScore() {
-    	
+
     	DynamicForm requestData = Form.form().bindFromRequest();
-    	
+
     	Long playerOneId = Long.parseLong(requestData.get("playerOne"));
     	Long playerTwoId = Long.parseLong(requestData.get("playerTwo"));
     	Integer playerOnePoints = Integer.parseInt(requestData.get("playerOnePoints"));
     	Integer playerTwoPoints = Integer.parseInt(requestData.get("playerTwoPoints"));
-    	
+
     	TennisGameService tgs = new TennisGameService();
     	tgs.storeNewMatch(playerOneId, playerTwoId, playerOnePoints, playerTwoPoints);
-    	
+
     	return redirect(routes.HomeController.index());
     }
-    
-    public Result insertPlayers() {
-    	
-    	if (Player.find.order("ranking desc").findList().isEmpty()) {
-    	
-	    	List<String> playerNames = Arrays.asList("Amar", "Emin", "Anis", "Jasmin", "Faruk", "Emir", "Kenan");
-	    	
-	    	for (String name : playerNames) {
-	    		
-		    	Player player = new Player();
-		    	player.name = name;
-		    	player.position = 1;
-		    	player.latestPositionDelta = 0;
-		    	player.ranking = 1000L;
-		    	player.matchesPlayed = 0;
-		    	player.winCount = 0;
-		    	player.lostCount = 0;
-		    	
-		    	player.save();
-	    	}
-	    	
-	    	return ok("Players Inserted: " + playerNames);
-	    	
-    	} else {
-    		return ok("Players already exist.");
-    	}
-    }
-    
-
 }
--- app/models/Player.java
+++ app/models/Player.java
@@ -15,7 +15,7 @@ public class Player extends Model {
 
 	@Id
 	public Long id;
-	
+
 	public String name;
 
 	public Integer position;
@@ -24,14 +24,33 @@ public class Player extends Model {
 	public Integer matchesPlayed;
 	public Integer winCount;
 	public Integer lostCount;
-	
+
 	@OneToMany(mappedBy = "playerOne", cascade=CascadeType.ALL, fetch = FetchType.LAZY)
 	public List<MatchScore> matchesAsPlayerOne;
 
 	@OneToMany(mappedBy = "playerTwo", cascade=CascadeType.ALL)
 	public List<MatchScore> matchesAsPlayerTwo;
-	
-	
+
+
 	public static Find<Long, Player> find = new Find<Long, Player>() {};
-	
+
+
+	/**
+	 * Creates new player instance with default paremters.
+	 * @param name {@link String} name of the player
+	 * @return {@link Player} player instance
+	 */
+	public static Player createWithDefaultParameters(String name) {
+		Player player = new Player();
+		player.name = name;
+		player.position = 1;
+		player.latestPositionDelta = 0;
+		player.ranking = 1000L;
+		player.matchesPlayed = 0;
+		player.winCount = 0;
+		player.lostCount = 0;
+
+		return player;
+	}
+
 }
